d996ce70e7818892601e2a46fa8d8f815ba64dd1
0xTiger <tgh.lizard@googlemail.com>
2022-02-20 17:17:00 +0000

    refactor: small cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
 use macroquad::prelude::*;
 
+const DEAD_CLR: Color = BLACK;
+const ALIVE_CLR: Color = WHITE;
+
+
+struct CellGrid {
+    data: Vec<u8>,
+    width: usize,
+    height: usize
+}
+
+impl CellGrid {
+    fn get(&self, x: usize, y: usize) -> u8{
+        let i = y * self.width + x;
+        self.data[i]
+    }
+
+    fn set(&mut self, x: usize, y: usize, color: u8){
+        let i = y * self.width + x;
+        self.data[i] = color;
+    }
+
+    fn new(&self, width: usize, height: usize) -> Self{
+        CellGrid { data: vec![0; height * width], 
+                    width: width, 
+                    height: height }
+    }
+}
+
 
 #[macroquad::main("cellular-automata")]
 async fn main() {
@@ -13,6 +41,7 @@ async fn main() {
         }
     }
     let mut fpss: Vec<i32> = Vec::new();
+    
     loop {
         clear_background(BLACK);
 
@@ -21,23 +50,20 @@ async fn main() {
             for y in 1..image.height - 1 {
                 let x = x.into();
                 let y = y.into();
-                let mut color = BLACK;
-                let alive = image.get_pixel(x, y) == WHITE;
+                let alive = image.get_pixel(x, y) == ALIVE_CLR;
                 let mut alive_neighbours = 0;
-                image.get_pixel(x, y);
 
-                let neigbours: &[(i32, i32)] = &[(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), 
+                let neigbours = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), 
                                             (1, -1), (1, 0), (1, 1)];
 
-                for &(a,b) in neigbours.iter() {
-                    
-                alive_neighbours += (image.get_pixel((x as i32 + a) as u32, (y as i32 + b) as u32) == WHITE) as u32;
+                for (a, b) in neigbours.iter() { 
+                    alive_neighbours += (image.get_pixel((x as i32 + a) as u32, (y as i32 + b) as u32) == WHITE) as u32;
                 }
 
                 if alive && !vec![2, 3].contains(&alive_neighbours){
-                    image.set_pixel(x, y, BLACK);
+                    image.set_pixel(x, y, DEAD_CLR);
                 } else if !alive && alive_neighbours == 3{
-                    image.set_pixel(x, y, WHITE);
+                    image.set_pixel(x, y, ALIVE_CLR);
                 }
             }
         }
@@ -47,7 +73,7 @@ async fn main() {
         //     zoom: Vec2::new(1.0, 1.0),
         //     ..Default::default()
         // };
-        set_camera(&Camera2D::from_display_rect(Rect::new(0.0, 0.0, 100.0, 100.0)));
+        set_camera(&Camera2D::from_display_rect(Rect::new(0.0, 0.0, image.width.into(), image.height.into())));
         draw_texture(texture, 0.0, 0.0, WHITE);
         fpss.push(get_fps());
         let l = fpss.len().saturating_sub(10);
